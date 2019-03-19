a9b22019f147247f5a34c3426abee9b7353af24b
Tagir <tagirshin@gmail.com>
2019-03-19 13:49:01 +0300

    Test file contains water

--- test.py
+++ test.py
@@ -1,17 +1,36 @@
+# -*- coding: utf-8 -*-
+#
+#  Copyright 2019 Ramil Nugmanov <stsouko@live.ru>
+#  Copyright 2019 Tagir Akhmetshin <tagirshin@gmail.com>
+#  This file is part of Molecule.
+#
+#  Molecule is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU Lesser General Public License as published by
+#  the Free Software Foundation; either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#  GNU Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public License
+#  along with this program; if not, see <https://www.gnu.org/licenses/>.
+#
+
 from Molecule.containers import graph
+from Molecule.periodictable.XVI import O
+from Molecule.periodictable.I import H
 
 m1 = graph.Graph()
+H = H()
+O = O()
 
-m1.add_atom('F')
-m1.add_atom('C')
-m1.add_atom('O')
-m1.add_atom('C')
-m1.add_atom('Cl')
+m1.add_atom(O)
+m1.add_atom(H)
+m1.add_atom(H)
 
 m1.add_bond(1, 2, 1)
-m1.add_bond(2, 3, 1)
-m1.add_bond(3, 4, 1)
-m1.add_bond(4, 5, 1)
-m1.add_bond(2, 4, 1)
+m1.add_bond(1, 3, 1)
 
 print(m1.clique())
