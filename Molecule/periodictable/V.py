3c07aa92013c5c558582ed033089c8a197cb0b6d
sashalands <47747263+sashalands@users.noreply.github.com>
2019-04-05 14:34:55 +0300

    Group V added (#40)
    * group II added
    
    * group II all added
    
    * group II updated
    
    * commas deleted
    
    * - V group added
    
    * - copyright fixed
    
    * - valences fixed
    
    * - multiplicities fixed
    
    * - multiplicities fixed
    
    * - multiplicities fixed

--- Molecule/periodictable/V.py
+++ Molecule/periodictable/V.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 #
 #  Copyright 2019 Ramil Nugmanov <stsouko@live.ru>
+#  Copyright 2019 Alexander Nikanshin <17071996sasha@gmail.com>
 #  Copyright 2019 Tagir Akhmetshin <tagirshin@gmail.com>
 #  This file is part of Molecule.
 #
@@ -25,4 +26,144 @@ class GroupV:
     pass
 
 
-__all__ = ['GroupV']
+class V(Element, PeriodIV, GroupV):
+    @property
+    def atomic_number(self):
+        return 23
+
+    @property
+    def atomic_mass(self):
+        return 50.9415
+
+    @property
+    def electronegativity(self):
+        return 1.63
+
+    @property
+    def common_isotope(self):
+        return 51
+
+    @property
+    def max_isotope(self):
+        return 51
+
+    @property
+    def min_isotope(self):
+        return 48
+
+    @property
+    def common_valences(self):
+        return (0, 4), (3, 1), (5, 1)
+
+    @property
+    def valences_exceptions(self):
+        return ((0, 4, ((1, 'F'), (1, 'F'))),
+                (0, 2, ((2, 'O'), (2, 'O'))),
+                (-4, 4, ((1, 'C'), (1, 'C'), (1, 'C'), (1, 'C'), (1, 'C'), (1, 'C'))))  # V(CN)6^4-
+
+
+class Nb(Element, PeriodV, GroupV):
+    @property
+    def atomic_number(self):
+        return 41
+
+    @property
+    def atomic_mass(self):
+        return 92.90638
+
+    @property
+    def electronegativity(self):
+        return 1.6
+
+    @property
+    def common_isotope(self):
+        return 93
+
+    @property
+    def max_isotope(self):
+        return 91
+
+    @property
+    def min_isotope(self):
+        return 95
+
+    @property
+    def common_valences(self):
+        return (0, 5), (5, 1)
+
+    @property
+    def valences_exceptions(self):
+        return ((0, 4, ((1, 'Br'), (1, 'Br'))),
+                (0, 3, ((1, 'F'), (1, 'F'), (1, 'F'))),
+                (0, 2, ((1, 'F'), (1, 'F'), (1, 'F'), (1, 'F'))))
+
+
+class Ta(Element, PeriodVI, GroupV):
+    @property
+    def atomic_number(self):
+        return 73
+
+    @property
+    def atomic_mass(self):
+        return 180.9478
+
+    @property
+    def electronegativity(self):
+        return 1.5
+
+    @property
+    def common_isotope(self):
+        return 181
+
+    @property
+    def max_isotope(self):
+        return 183
+
+    @property
+    def min_isotope(self):
+        return 179
+
+    @property
+    def common_valences(self):
+        return (0, 4), (3, 1), (5, 1)
+
+    @property
+    def valences_exceptions(self):
+        return ()
+
+
+class Db(Element, PeriodVII, GroupV):
+    @property
+    def atomic_number(self):
+        return 105
+
+    @property
+    def atomic_mass(self):
+        return 268
+
+    @property
+    def electronegativity(self):
+        return None
+
+    @property
+    def common_isotope(self):
+        return 268
+
+    @property
+    def max_isotope(self):
+        return 268
+
+    @property
+    def min_isotope(self):
+        return 268
+
+    @property
+    def common_valences(self):
+        return ()
+
+    @property
+    def valences_exceptions(self):
+        return ()
+
+
+__all__ = ['GroupV', 'V', 'Nb', 'Ta', 'Db']
