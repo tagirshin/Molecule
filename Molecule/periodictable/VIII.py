2a479865728a33188aa050f540ae9fbab8e90201
dayanabashirova <47747228+dayanabashirova@users.noreply.github.com>
2019-04-19 10:18:39 +0300

    VII (#61)
    * Fe added
    
    * Fe, Ru added

--- Molecule/periodictable/VIII.py
+++ Molecule/periodictable/VIII.py
@@ -25,4 +25,98 @@ class GroupVIII:
     pass
 
 
-__all__ = ['GroupVIII']
+class Fe(Element, PeriodIV, GroupVIII):
+    @property
+    def atomic_number(self):
+        return 26
+
+    @property
+    def atomic_mass(self):
+        return 55.845
+
+    @property
+    def electronegativity(self):
+        return 1.83
+
+    @property
+    def common_isotope(self):
+        return 56
+
+    @property
+    def max_isotope(self):
+        return 60
+
+    @property
+    def min_isotope(self):
+        return 54
+
+    @property
+    def common_valences(self):
+        return (0, 5), (2, 5), (3, 6)
+
+    @property
+    def valences_exceptions(self):
+        return ((-2, 5, ((1, 'O'), (1, 'O'), (1, 'O'), (1, 'O'))),  # [Fe(OH)4]2-
+                (-4, 5, ((1, 'O'), (1, 'O'), (1, 'O'), (1, 'O'), (1, 'O'), (1, 'O'))),  # [Fe(OH)6]4-
+                (-4, 5, ((2, 'O'), (2, 'O'), (2, 'O'))),  # [FeO3]4-
+                (-2, 5, ((1, 'F'), (1, 'F'), (1, 'F'), (1, 'F'))),  # [FeF4]2-
+                (-2, 5, ((1, 'Cl'), (1, 'Cl'), (1, 'Cl'), (1, 'Cl'))),  # [FeCl4]2-
+                (-4, 5, ((1, 'C'), (1, 'C'), (1, 'C'), (1, 'C'), (1, 'C'), (1, 'C'))),  # [Fe(CN)6]4-
+                (-4, 5, ((1, 'S'), (1, 'S'), (1, 'S'), (1, 'S'), (1, 'S'), (1, 'S'))),  # [Fe(SCN)6]4-
+                (-4, 5, ((1, 'N'), (1, 'N'), (1, 'N'), (1, 'N'), (1, 'N'), (1, 'N'))),  # [Fe(NCS)6]4-
+                (-2, 5, ((1, 'S'), (1, 'S'), (1, 'S'), (1, 'S'))),  # [Fe(SCN)4]2-
+                (-4, 5, ((1, 'N'), (1, 'N'), (1, 'N'), (1, 'N'))),  # [Fe(NCS)4]2-
+                (-3, 6, ((1, 'S'), (1, 'S'), (1, 'S'), (1, 'S'), (1, 'S'), (1, 'S'))),  # [Fe(SCN)6]3-
+                (-3, 6, ((1, 'N'), (1, 'N'), (1, 'N'), (1, 'N'), (1, 'N'), (1, 'N'))),  # [Fe(NCS)6]3-
+                (-3, 6, ((1, 'C'), (1, 'C'), (1, 'C'), (1, 'C'), (1, 'C'), (1, 'C'))),  # [Fe(CN)6]3-
+                (-1, 6, ((1, 'Cl'), (1, 'Cl'), (1, 'Cl'), (1, 'Cl'))),  # [FeCl4]-
+                (-2, 6, ((1, 'Cl'), (1, 'Cl'), (1, 'Cl'), (1, 'Cl'), (1, 'Cl'))),  # [FeCl5]2-
+                (-3, 6, ((1, 'Cl'), (1, 'Cl'), (1, 'Cl'), (1, 'Cl'), (1, 'Cl'), (1, 'Cl'))),  # [FeCl6]3-
+                (-3, 6, ((1, 'F'), (1, 'F'), (1, 'F'), (1, 'F'), (1, 'F'), (1, 'F'))),  # [FeF6]3-
+                (-1, 6, ((1, 'Br'), (1, 'Br'), (1, 'Br'), (1, 'Br'))))  # [FeBr4]-
+
+
+
+class Ru(Element, PeriodV, GroupVIII):
+    @property
+    def atomic_number(self):
+        return 44
+
+    @property
+    def atomic_mass(self):
+        return 101.07
+
+    @property
+    def electronegativity(self):
+        return 2.2
+
+    @property
+    def common_isotope(self):
+        return 102
+
+    @property
+    def max_isotope(self):
+        return 106
+
+    @property
+    def min_isotope(self):
+        return 96
+
+    @property
+    def common_valences(self):
+        return (0, 4), (2, 5), (3, 6), (4, 5), (6, 3)
+
+    @property
+    def valences_exceptions(self):
+        return ((0, 1, ((2, 'O'), (2, 'O'), (2, 'O'), (2, 'O'))),  # RuO4
+                (-1, 2, ((2, 'O'), (2, 'O'), (2, 'O'), (2, 'O'))),  # [RuO4]-
+                (-3, 4, ((2, 'O'), (2, 'O'), (2, 'O'), (2, 'O'))),  # [RuO4]3-
+                (-2, 6, ((1, 'Cl'), (1, 'Cl'), (1, 'Cl'), (1, 'Cl'), (1, 'Cl'))),  # [RuCl5]2-
+                (-3, 6, ((1, 'Cl'), (1, 'Cl'), (1, 'Cl'), (1, 'Cl'), (1, 'Cl'), (1, 'Cl'))),  # [RuCl6]3-
+                (-2, 5, ((1, 'Cl'), (1, 'Cl'), (1, 'Cl'), (1, 'Cl'), (1, 'Cl'), (1, 'Cl'))),  # [RuCl6]2-
+                (-4, 6, ((1, 'Cl'), (1, 'Cl'), (1, 'Cl'), (1, 'Cl'), (1, 'Cl'), (1, 'Cl'), (1, 'Cl'))),  # [RuCl7]4-
+                (-2, 5, ((1, 'F'), (1, 'F'), (1, 'F'), (1, 'F'), (1, 'F'), (1, 'F'))),  # [RuF6]2-
+                (-1, 4, ((1, 'F'), (1, 'F'), (1, 'F'), (1, 'F'), (1, 'F'), (1, 'F'))))  # [RuF6]-
+
+
+__all__ = ['GroupVIII', 'Fe', 'Ru']
